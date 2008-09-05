fcf465c1f3a83a262da906a8ba33c771a42e2da7
Mike Schwab <mike.schwab@gmail.com>
2008-09-04 22:29:53 -0400

    added not_blank?, removed #all because it has been added to rails core, added AR::Base#dom_id

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -10,15 +10,29 @@ class ApplicationController < ActionController::Base
 end
 
 class ActiveRecord::Base
-  def self.all conditions = {}; find :all, :conditions => conditions;end
+  def dom_id prefix = nil; ActionView::Base.new.dom_id self, prefix; end
+  # def self.all conditions = {}; find :all, :conditions => conditions;end
   def self.random amount = 1; find :all, :limit => amount, :order => 'rand()' ;end
   def age; Time.now - created_at ;end  
   before_create :credit_creator
   def credit_creator; self.railser_id ||= current_railser_id || 1 ;end
   def current_railser_id; @railser_id ;end
 end
-class Array; def randomize; sort_by { rand } ;end;end
+class Array
+  def randomize; sort_by { rand } ;end
+  def not_blank?; not blank? ;end
+end
 class String; def urlize; gsub(/\s+/, '_').gsub(/\.\?,\//, '~') unless blank? ;end
-  def wrap_for_code first; return self[/#{first.to_s}[*,]\d+[*,]\d+/].gsub(/[*,]/, '') if self[/#{first.to_s}[*,]\d+[*,]\d+/]
-                           return self[/#{first.to_s}[*,]\d+/].concat(self[/^\d+/]).gsub(/[*,]/, '') if self[/#{first.to_s}[*,]\d+/]
-                           return self[/#{first.to_s}/].concat(self[/^\d+[*,]\d+/]).gsub(/[*,]/, '') if self[/#{first.to_s}[*,]?$/] ;end;end
+  def wrap_for_code first
+    return self[/#{first.to_s}[*,]\d+[*,]\d+/].gsub(/[*,]/, '') if self[/#{first.to_s}[*,]\d+[*,]\d+/]
+    return self[/#{first.to_s}[*,]\d+/].concat(self[/^\d+/]).gsub(/[*,]/, '') if self[/#{first.to_s}[*,]\d+/]
+    return self[/#{first.to_s}/].concat(self[/^\d+[*,]\d+/]).gsub(/[*,]/, '') if self[/#{first.to_s}[*,]?$/]
+  end
+  def not_blank?; not blank? ;end
+end
+class NilClass
+  def not_blank?; not blank? ;end
+end
+class FixNum
+  def not_blank?; not blank? ;end
+end
