6daae03e637df89b4705e4ef7db59441dcd3ee72
Mike Schwab <mike.schwab@gmail.com>
2008-10-24 18:28:03 -0400

    changed production environment

--- spec/models/railevance_spec.rb
+++ spec/models/railevance_spec.rb
@@ -7,27 +7,27 @@ describe Railevance do
       @rails = Concept.create(:content => 'rails')
       @ruby = Concept.create(:content => 'ruby')
       @rails_ruby = Railevance.create(:rail_id => @rails.id, :tie_id => @ruby.id)
-      @dhh = Concept.create(:content => 'DHH')
-      @dhh_eureka = Railevance.create(:rail_id => @dhh.id, :tie_r_id => @rails_ruby.id)
+      @textmate = Concept.create(:content => 'TextMate')
+      @bundle = Railevance.create(:rail_id => @textmate.id, :tie_r_id => @rails_ruby.id)
     end
     
     it 'should call cache_in_components when created' do
-      railevance = Railevance.new(:rail_id => @rails.id, :tie_r_id => @dhh_eureka.id)
+      railevance = Railevance.new(:rail_id => @rails.id, :tie_r_id => @bundle.id)
       flexmock railevance
       railevance.should_receive(:cache_in_components).once
       railevance.save
     end
     
     it 'should call cache_connections on the right object with the right id hash' do
-      railevance = Railevance.new :rail_id => @rails.id, :tie_r_id => @dhh_eureka.id
+      railevance = Railevance.new :rail_id => @rails.id, :tie_r_id => @bundle.id
       flexmock @rails
-      @rails.should_receive(:cache_connections).with({:tie_r => @dhh_eureka.id}).once
+      @rails.should_receive(:cache_connections).with({:tie_r => @bundle.id}).once
       railevance.save
     end
     
     it 'should call extant_connections when cache_in_components is called' do
-      railevance = flexmock(Railevance.new(:rail_id => @rails.id, :tie_r_id => @dhh_eureka.id))
-      railevance.should_receive(:extant_connections).and_return({:rail => @rails.id, :tie_r => @dhh_eureka.id}).at_least.once
+      railevance = flexmock(Railevance.new(:rail_id => @rails.id, :tie_r_id => @bundle.id))
+      railevance.should_receive(:extant_connections).and_return({:rail => @rails.id, :tie_r => @bundle.id}).at_least.once
       railevance.cache_in_components
     end
     
@@ -45,7 +45,7 @@ describe Railevance do
     end
     
     it 'should cache ids in connected railevances' do
-      @dhh.cached_railevance_ids.should == [@rails_ruby.id]
+      @textmate.cached_railevance_ids.should == [@rails_ruby.id]
     end
     
     it 'should know that it has two extant connections' do
@@ -55,8 +55,8 @@ describe Railevance do
     it 'should have the correct keys in extant connections' do
       @rails_ruby.extant_connections.keys.include?(:rail).should == true
       @rails_ruby.extant_connections.keys.include?(:tie).should == true
-      @dhh_eureka.extant_connections.keys.include?(:rail).should == true
-      @dhh_eureka.extant_connections.keys.include?(:tie_r).should == true
+      @bundle.extant_connections.keys.include?(:rail).should == true
+      @bundle.extant_connections.keys.include?(:tie_r).should == true
     end
   end
 end
