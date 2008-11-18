0a9d8add55691ced33353b66a4cfa31652d17922
Mike Schwab <mike.schwab@gmail.com>
2008-11-18 12:26:31 -0500

    removed logic from migration that deals with existing data

--- db/migrate/005_cache_railevances.rb
+++ db/migrate/005_cache_railevances.rb
@@ -1,25 +1,25 @@
-class Concept < ActiveRecord::Base
-end
-class Library < Concept
-end
-class Tool < Concept
-end
-class Feature < Concept
-end
-class Directory < Concept
-end
-class Railevance < ActiveRecord::Base
-end
+# class Concept < ActiveRecord::Base
+# end
+# class Library < Concept
+# end
+# class Tool < Concept
+# end
+# class Feature < Concept
+# end
+# class Directory < Concept
+# end
+# class Railevance < ActiveRecord::Base
+# end
 class CacheRailevances < ActiveRecord::Migration
   def self.up
     add_column :concepts, :rails_ids, :text
     add_column :concepts, :ties_ids, :text
   
-    Railevance.find(:all).each do |r|
-      (rail = Concept.find(r.rail_id)).update_attribute(:ties_ids, (rail.ties_ids ? (rail.ties_ids + ' ') : '').concat(r.tie_id.to_s))
-      (tie = Concept.find(r.tie_id)).update_attribute(:rails_ids, (tie.rails_ids ? (tie.rails_ids + ' ') : '').concat(r.rail_id.to_s))
-      rail.save; tie.save
-    end
+    # Railevance.find(:all).each do |r|
+    #   (rail = Concept.find(r.rail_id)).update_attribute(:ties_ids, (rail.ties_ids ? (rail.ties_ids + ' ') : '').concat(r.tie_id.to_s))
+    #   (tie = Concept.find(r.tie_id)).update_attribute(:rails_ids, (tie.rails_ids ? (tie.rails_ids + ' ') : '').concat(r.rail_id.to_s))
+    #   rail.save; tie.save
+    # end
     
   end
 
