3c68f2674bd3ea0053f214a45f0369dbdbbf8fc9
Mike Schwab <mike.schwab@gmail.com>
2008-11-21 17:07:11 -0500

    added login links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def login_stuff
+    if logged_in?
+      link_to 'logout', logout_path
+    else
+      link_to('signup', signup_path) +
+      link_to('login', login_path)
+    end
+  end
 end
